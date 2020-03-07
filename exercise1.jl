ef6ab52a97ee4413396ae7e30f92b2ae7a2a9ab7
nikos6ares <61912357+nikos6ares@users.noreply.github.com>
2020-03-07 16:37:17 +0100

    Update and rename commandsCSP.jl to exercise1.jl

---
+++ exercise1.jl
@@ -0,0 +1,14 @@
+using Printf "to use the python command {} {} .float{etc blah}"
+using Plots "to plot"
+
+@time begin   " to calculate the execution time of blah"
+blah
+end
+
+function example(arguments)
+  "equivalently to define in Python"
+end
+
+mod(z,L) "It is a nice way to consider periodic boundary conditions"
+
+Array(1:2:10) "Creates an 
